fd647939b774f2fcd63c7b04ee63b170e52bc320
Etienne <gwiavanderetienne2@gmail.com>
2023-03-21 18:59:38 +0100

    news section

---
+++ src/news/useFetchArray.js
@@ -0,0 +1,51 @@
+import { useEffect, useState  } from "react";
+import { API_key } from "../api";
+
+const useFetch = (url) => {
+
+    const [fetchedData, setFetchedData]= useState([])
+    const [isLoading, setIsLoading]= useState(true)
+    const [error, setError]= useState(null)
+
+    const options = {
+        headers: {
+            'X-RapidAPI-Key':`${API_key}` ,
+        }
+    };
+    
+    useEffect (()=>{
+  
+        const getData = setTimeout(() => {
+            fetch(url, options)
+            .then(response => {
+                
+                if(!response.ok){
+                    throw Error("Could not fetch the data")
+                }
+                return response.json()})
+            .then(data => {
+                
+                setFetchedData(data)
+                setIsLoading(false)
+                setError(null)
+            })
+            .catch(err => {
+                if(err.name === "AbortError"){
+                
+                }else{
+                setError(err.message)
+                setIsLoading(false)
+                setFetchedData(null)  
+                setFetchedData(false)        
+                }
+        
+            })
+        }, 1500)
+        
+        return () => clearTimeout(getData)
+    }, [url])
+
+    return {fetchedData, isLoading, error}
+}
+ 
+export default useFetch;
